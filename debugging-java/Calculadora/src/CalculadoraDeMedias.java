4bc7523621f915d7a7004c2fcf7fcb8097beed42
thekaiofc <kaio.hen13@gmail.com>
2024-07-23 21:32:29 -0300

    Aulas sobre depuração de código nas IDE's (debugging java).

---
+++ debugging-java/Calculadora/src/CalculadoraDeMedias.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class CalculadoraDeMedias {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] alunos = {"Camila", "Lucas", "Bruna", "Pedro"};
+
+        double media = calculaMediaDaTurma(alunos, scan);
+
+        System.out.println("Média da turma "+ media);
+    }
+
+    static double calculaMediaDaTurma(String[] alunos, Scanner scanner){
+        double soma = 0;
+        for(String aluno : alunos){
+            System.out.printf("Nota do aluno %s:" , aluno);
+
+            double nota = scanner.nextDouble();
+            soma += nota;
+        }
+
+        return soma / alunos.length;
+    }
+}
