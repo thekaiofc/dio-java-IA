c507e23d90f0118eb85461ca263c485b85b7f32e
thekaiofc <kaio.hen13@gmail.com>
2024-08-05 20:24:46 -0300

    Exemplo de Herança na POO

---
+++ exemplo-pacotes/src/MSNMessenger.java
@@ -0,0 +1,5 @@
+
+public class MSNMessenger extends ServicoMensagemInstantanea {
+	
+
+}
---
+++ exemplo-pacotes/src/ServicoMensagemInstantanea.java
@@ -0,0 +1,17 @@
+
+public class ServicoMensagemInstantanea {
+	public void enviarMensagem() {
+		validarConectadoInternet();
+		System.out.println("Enviando mensagem.");
+		salvarHistoricoMensagem();
+	}
+	public void receberMensagem() {
+		System.out.println("Recebendo mensagem.");
+	}
+	private void validarConectadoInternet() {
+		System.out.println("Validando se está conectado a internet.");
+	}
+	private void salvarHistoricoMensagem() {
+		System.out.println("Salvando histórico da mensagem");
+	}
+}
