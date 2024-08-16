d39acc269e285afc6f60b0d4374b5092b6c21e6e
thekaiofc <kaio.hen13@gmail.com>
2024-08-15 22:45:16 -0300

    Dando os primeiros passos no Spring Framework (Spring Boot)

---
+++ primeiros-passos/src/main/java/dio/springboot/Calculadora.java
@@ -0,0 +1,11 @@
+package dio.springboot;
+
+public class Calculadora {
+
+    private int num1;
+    private int num2;
+
+    public int somar(int num1, int num2){
+        return num1 + num2;
+    }
+}
